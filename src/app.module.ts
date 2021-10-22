bec701dc6f5023a8bed897c17ba668435a2662dd
Vinicius Avena <vini.avena@live.com>
2021-10-22 18:10:03 -0300

    prisma schema done

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DoctorsModule } from './doctors/doctors.module';
 
 @Module({
-  imports: [],
+  imports: [DoctorsModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/doctors/dto/create-doctor.dto.ts
@@ -0,0 +1,5 @@
+export class CreateDoctorDto {
+    fullName: string;
+    crm: string;
+    speciality: string;
+}
