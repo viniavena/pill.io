119082a1a42d0b5f2912f274f9041c1c73a08141
Vinicius Avena <vini.avena@live.com>
2021-10-22 22:27:33 -0300

    doctors CRUD done

--- src/doctors/entities/doctor.entity.ts
+++ src/doctors/entities/doctor.entity.ts
@@ -1,1 +1,10 @@
-export class Doctor {}
+import {Prisma} from '@prisma/client'
+
+export class Doctor implements Prisma.DoctorUncheckedCreateInput{
+    createdAt?: string | Date;
+    crm: string;
+    pacients?: Prisma.PacientUncheckedCreateNestedManyWithoutDoctorInput;
+    prescription?: Prisma.PrescriptionUncheckedCreateNestedManyWithoutDoctorInput;
+    id?: number;
+    fullName: string;
+}
